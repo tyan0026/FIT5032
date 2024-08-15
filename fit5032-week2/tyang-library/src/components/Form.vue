c24048770eb44d5bf0447f68cc90ab1cc2bfd44e
tyan0026 <tyan0026@student.monash.edu>
2024-08-15 21:32:17 +1000

    Display User Information with PrimeVue DataTable

--- fit5032-week2/tyang-library/src/components/Form.vue
+++ fit5032-week2/tyang-library/src/components/Form.vue
@@ -7,20 +7,28 @@
           <div class="row mb-3">
             <div class="col-md-4">
               <label for="username" class="form-label">Username</label>
-              <input type="text" class="form-control" id="username"
+              <input
+                type="text"
+                class="form-control"
+                id="username"
                 @blur="() => validateName(true)"
                 @input="() => validateName(false)"
-                v-model="formData.username" />
-                <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
+                v-model="formData.username"
+              />
+              <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
             </div>
             <div class="col-md-4">
               <label for="password" class="form-label">Password</label>
-              <input type="password" class="form-control" id="password"
-                   @blur="() => validatePassword(true)"
-                   @input="() => validatePassword(false)"
-                   v-model="formData.password" />
-            <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
-        </div>
+              <input
+                type="password"
+                class="form-control"
+                id="password"
+                @blur="() => validatePassword(true)"
+                @input="() => validatePassword(false)"
+                v-model="formData.password"
+              />
+              <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
+            </div>
             <div class="col-md-4">
               <label for="gender" class="form-label">Gender</label>
               <select class="form-select" id="gender" v-model="formData.gender">
@@ -42,7 +50,6 @@
                 <label class="form-check-label" for="isAustralian">Australian Resident?</label>
               </div>
             </div>
-           
           </div>
           <div class="mb-3">
             <label for="reason" class="form-label">Reason for joining</label>
@@ -62,28 +69,14 @@
           </div>
         </form>
       </div>
-      <div class="row mt-5" v-if="submittedCards.length">
-        <div class="col-md-12">
-          <div class="d-flex flex-wrap justify-content-start">
-            <div
-              v-for="(card, index) in submittedCards"
-              :key="index"
-              class="card m-2"
-              style="width: 18rem"
-            >
-              <div class="card-header">User Information</div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Username: {{ card.username }}</li>
-                <li class="list-group-item">Password: {{ card.password }}</li>
-                <li class="list-group-item">
-                  Australian Resident: {{ card.isAustralian ? 'Yes' : 'No' }}
-                </li>
-                <li class="list-group-item">Gender: {{ card.gender }}</li>
-                <li class="list-group-item">Reason: {{ card.reason }}</li>
-              </ul>
-            </div>
-          </div>
-        </div>
+      <div v-if="submittedCards.length > 0">
+        <DataTable :value="submittedCards" tableStyle="min-width: 50rem">
+          <Column field="username" header="User Name"></Column>
+          <Column field="password" header="Password"></Column>
+          <Column field="isAustralian" header="isAustralian"></Column>
+          <Column field="reason" header="Reason of Join"></Column>
+          <Column field="gender" header="Gender"></Column>
+        </DataTable>
       </div>
     </div>
   </div>
@@ -92,6 +85,8 @@
 <script setup>
 // Our logic will go here
 import { ref } from 'vue'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
 
 const formData = ref({
   username: '',
@@ -104,13 +99,12 @@ const formData = ref({
 const submittedCards = ref([])
 
 const submitForm = () => {
-    validateName(true);
-    validatePassword(true);
-    if(!errors.value.username && !errors.value.password && !errors.value.reason){
-        submittedCards.value.push({ ...formData.value });
-        clearForm
-    }
-
+  validateName(true)
+  validatePassword(true)
+  if (!errors.value.username && !errors.value.password && !errors.value.reason) {
+    submittedCards.value.push({ ...formData.value })
+    clearForm
+  }
 }
 
 const clearForm = () => {
@@ -122,54 +116,54 @@ const clearForm = () => {
 }
 
 const errors = ref({
-    username: null,
-    password: null,
-    resident: null,
-    gender: null,
-    reason: null,
+  username: null,
+  password: null,
+  resident: null,
+  gender: null,
+  reason: null
 })
 
 const validateName = (blur) => {
-    if (formData.value.username.length < 3){
-        if(blur)errors.value.username = "Name must be at least 3 characters";
-    }else{
-        errors.value.username = null;
-    }
+  if (formData.value.username.length < 3) {
+    if (blur) errors.value.username = 'Name must be at least 3 characters'
+  } else {
+    errors.value.username = null
+  }
 }
 
 const validatePassword = (blur) => {
-    const password = formData.value.password;
-    const minLength = 8;
-    const hasUppercase = /[A-Z]/.test(password);
-    const hasLowercase = /[a-z]/.test(password);
-    const hasNumber = /\d/.test(password);
-    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+  const password = formData.value.password
+  const minLength = 8
+  const hasUppercase = /[A-Z]/.test(password)
+  const hasLowercase = /[a-z]/.test(password)
+  const hasNumber = /\d/.test(password)
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password)
 
-    if (password.length < minLength) {
-        if (blur) errors.value.password = `Password must be at least ${minLength} characters long.`;
-    } else if (!hasUppercase) {
-        if (blur) errors.value.password = "Password must contain at least one uppercase letter.";
-    } else if (!hasLowercase) {
-        if (blur) errors.value.password = "Password must contain at least one lowercase letter.";
-    } else if (!hasNumber) {
-        if (blur) errors.value.password = "Password must contain at least one number.";
-    } else if (!hasSpecialChar) {
-        if (blur) errors.value.password = "Password must contain at least one special character.";
-    } else {
-        errors.value.password = null;
-    }
-};
+  if (password.length < minLength) {
+    if (blur) errors.value.password = `Password must be at least ${minLength} characters long.`
+  } else if (!hasUppercase) {
+    if (blur) errors.value.password = 'Password must contain at least one uppercase letter.'
+  } else if (!hasLowercase) {
+    if (blur) errors.value.password = 'Password must contain at least one lowercase letter.'
+  } else if (!hasNumber) {
+    if (blur) errors.value.password = 'Password must contain at least one number.'
+  } else if (!hasSpecialChar) {
+    if (blur) errors.value.password = 'Password must contain at least one special character.'
+  } else {
+    errors.value.password = null
+  }
+}
 
 const validateReason = (blur) => {
-    const password = formData.value.reason;
-    const maxLength = 200;
+  const password = formData.value.reason
+  const maxLength = 200
 
-    if (password.length > maxLength) {
-        if (blur) errors.value.reason = `Reason of join shall not exceed ${maxLength} words.`;
-    } else {
-        errors.value.reason = null;
-    }
-};
+  if (password.length > maxLength) {
+    if (blur) errors.value.reason = `Reason of join shall not exceed ${maxLength} words.`
+  } else {
+    errors.value.reason = null
+  }
+}
 </script>
 
 <style scoped>

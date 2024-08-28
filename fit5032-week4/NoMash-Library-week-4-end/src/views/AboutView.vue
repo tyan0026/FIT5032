64c614588394996971269c3b64d1a011d2a0fdbb
tyan0026 <tyan0026@student.monash.edu>
2024-08-28 14:03:43 +1000

    add router for project

---
+++ fit5032-week4/NoMash-Library-week-4-end/src/views/AboutView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="about">
+      <h1>About Our Library</h1>
+      <p>Welcome to our digital library! We're dedicated to providing a vast collection of books and resources to our community.</p>
+    </div>
+  </template>
+  
+  <script setup>
+  // No script needed for now
+  </script>
+  
+  <style>
+  .about {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  margin-top: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+}
+
+.about h1, .about p {
+  margin: 10px 0;
+}
+  </style>
